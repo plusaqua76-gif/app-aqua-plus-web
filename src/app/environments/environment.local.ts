188329c8c8c33aeeec944f74c9a56952e9918d06
andreschavarro <114626513+andreschavarro@users.noreply.github.com>
2026-01-23 09:08:24 -0500

    fix(environment): update apiUrl to use production endpoint

--- src/app/environments/environment.local.ts
+++ src/app/environments/environment.local.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'https://app-aqua-plus-api.azurewebsites.net/api/v1',
-    apiUrl: 'http://localhost:8080/api/v1',
+    apiUrl: 'https://app-aqua-plus-api.azurewebsites.net/api/v1',
     azureBlobStorageUrl: 'https://staquaplus.blob.core.windows.net/document-public/',
     alegraSandbox:'https://sandbox-api.alegra.com/e-provider/col/v1/'
   };
