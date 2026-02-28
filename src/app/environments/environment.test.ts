dabd16c53c64cca56a1826872092c29b20df2325
andreschavarro <114626513+andreschavarro@users.noreply.github.com>
2026-02-28 10:15:56 -0500

    feat: add test configuration and environment settings for testing

---
+++ src/app/environments/environment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://app-aqua-plus-api-test.azurewebsites.net/api/v1',
+  azureBlobStorageUrl: 'https://staquaplus.blob.core.windows.net/document-public/'
+};
