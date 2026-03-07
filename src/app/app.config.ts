5faa7597b4026f5aabe9fbc2bf158c3ab6fca8c3
dchavarror <dchavarror>
2026-03-07 07:00:46 -0500

    Ajuste config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode, APP_INITIALIZER } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
@@ -9,12 +9,6 @@ import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/
 import { authorizationInterceptor } from './interceptors/token-interceptor';
 import { loaderInterceptor } from './interceptors/loader-interceptor';
 import { errorInterceptor } from './interceptors/error-interceptor';
-
-import { AppConfigService } from './config/app-config.service';
-
-export function initializeApp(configService: AppConfigService) {
-  return () => configService.getConfig();
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -33,12 +27,5 @@ export const appConfig: ApplicationConfig = {
       withIncrementalHydration(),
       withEventReplay()
     ),
-
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeApp,
-      deps: [AppConfigService],
-      multi: true
-    }
   ]
 };
